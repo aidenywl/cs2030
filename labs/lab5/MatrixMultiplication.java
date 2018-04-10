04fcdfd9524dedf5986fbe01952146b8e8b5a517
Low Yew Woei <yewwoei.low@u.nus.edu>
2018-04-10 09:54:03 +0800

    starting lab5

---
+++ labs/lab5/MatrixMultiplication.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.RecursiveTask;
+
+class MatrixMultiplication extends RecursiveTask<Matrix> {
+  public static final long serialVersionUID = 1;
+  // TODO
+
+  @Override
+  public Matrix compute() {
+    // TODO
+    return new Matrix(1);
+  }
+}
