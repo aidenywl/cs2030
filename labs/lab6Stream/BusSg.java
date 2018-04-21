f1be823defebcad52a4d084a9c4b318b174a474a
Low Yew Woei <yewwoei.low@u.nus.edu>
2018-04-21 22:57:05 +0800

    finished lab 6

--- labs/lab6Stream/BusSg.java
+++ labs/lab6Stream/BusSg.java
@@ -32,7 +32,7 @@ class BusSg {
       // get the bus services asynchronously.
       CompletableFuture<Set<BusService>> allServicesHelp = CompletableFuture
               .supplyAsync(() -> stop.getBusServices())
-              .handle((result, exception) -> { // handling HTTP exceptions.
+              .handle((result, exception) -> {
                   if (result != null) {
                     return result;
                   } else {
@@ -45,15 +45,10 @@ class BusSg {
       // Set<BusService> allServices = stop.getBusServices(); // <- TODO: call this asynchronously
       Map<BusService,Set<BusStop>> validServices = new HashMap<>();
 
-      /** Stores all the asynchronous tasks later on. */
-      ArrayList<CompletableFuture<Set<BusStop>>> tasks = new ArrayList<>();
-      
-      // retrieve the bus services that serve that bus stop.
+
       Set<BusService> allServices = allServicesHelp.join();
+      ArrayList<CompletableFuture<Set<BusStop>>> tasks = new ArrayList<>();
 
-
-
-      /* Stream version. Which is better?
       allServices.stream()
            .forEach(bus ->
               {
@@ -67,28 +62,10 @@ class BusSg {
                                 }
                              }));
               });
-      */
-      
-      // asynchronous request for all the URL API queries.
-      for (BusService service : allServices) {
-        // Set<BusStop> stops = service.findStopsWith(name);  // <- TODO: call this asynchronously
-        CompletableFuture<Set<BusStop>> futureStop = CompletableFuture
-            .supplyAsync(() -> service.findStopsWith(name))
-            .handle((result, exception) -> { // handling HTTP exceptions
-              if (result != null) {
-                return result;
-              } else {
-                System.err.println(exception);
-                return Collections.<BusStop>emptySet();
-              }
-            });
-          tasks.add(futureStop); // add the future to the queue.
 
-      }
-      // iterator to be used to aid the bus Services.
+
       Iterator<BusService> busServices = allServices.iterator();
-
-      // retrieving the information from each asynchronous task and using them.
+      // using the information after fetching everything
       for (CompletableFuture<Set<BusStop>> asyncStop : tasks) {
         BusService s = busServices.next();
         Set<BusStop> stops = asyncStop.join();
